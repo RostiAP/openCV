773707796de2d036547e231d4e0b52046148ebaa
Rosti <rostislaw.neumann@aaronprojects.de>
2020-09-23 16:52:52 +0200

    initial commit

---
+++ treshholding.py
@@ -0,0 +1,29 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+
+x, y, w, h = 270, 190, 100, 100
+white = (255, 255, 255)
+
+lower_color = (130, 140, 140)
+upper_color = (140, 150, 150)
+
+while cam.isOpened():
+    ret, frame = cam.read()
+    if not ret:
+        raise ValueError('No video')
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    cv2.rectangle(frame, (x, y), (x+h, y+w), white, thickness=1)
+    cv2.putText(frame, "HSV: {0}".format(frame[y+50, x+50]), (x, y+150),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, white, thickness=2)
+
+    mask = cv2.inRange(frame, lower_color, upper_color)
+
+    cv2.imshow('Video', frame)
+    cv2.imshow('Mask', mask)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
